ca21e8b816a9e31b6893cb8cd58450bf71475623
Satvik370 <95036017+Satvik370@users.noreply.github.com>
2021-11-26 17:05:54 +0530

    Add files via upload

--- Browser.py
+++ Browser.py
@@ -1,15 +1,38 @@
 import sys
+from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtWebEngineWidgets import *
 
-class Mainwindow(QMainWindow):
+
+class MainWindow(QMainWindow):
     def __init__(self):
-        super(Mainwindow, self).__init__()
-        self.browser =
+        super(MainWindow, self).__init__()
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl('http://google.com'))
+        self.setCentralWidget(self.browser)
         self.showMaximized()
 
+        # navbar
+        navbar = QToolBar()
+        self.addToolBar(navbar)
+
+        back_btn = QAction('Back', self)
+        back_btn.triggered.connect(self.browser.back)
+        navbar.addAction(back_btn)
+
+        forward_btn = QAction('Forward', self)
+        forward_btn.triggered.connect(self.browser.forward)
+        navbar.addAction(forward_btn)
+
+        reload_btn = QAction('Reload', self)
+        reload_btn.triggered.connect(self.browser.reload)
+        navbar.addAction(reload_btn)
+
+        home_btn = QAction('Home', self)
+        home_btn.triggered.connect(self.navigate_home)
+        navbar.addAction(home_btn)
 
 app = QApplication(sys.argv)
-QApplication.setApplicationName("Pybrowser")
-window=Mainwindow()
+QApplication.setApplicationName('Pybroswer')
+window = MainWindow()
 app.exec_()
