7558a282839c64b2307a098e73c7d987fce80011
Satvik370 <95036017+Satvik370@users.noreply.github.com>
2021-12-07 19:15:30 +0530

    Update Browser.py

--- Browser.py
+++ Browser.py
@@ -1,7 +1,6 @@
 import sys
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
 from PyQt5.QtWebEngineWidgets import *
 
 
@@ -9,56 +8,48 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.browser = QWebEngineView()
-        self.browser.setUrl(QUrl('http://duckduckgo.com'))
+        self.browser.setUrl(QUrl('https://duckduckgo.com/'))
         self.setCentralWidget(self.browser)
         self.showMaximized()
 
-        # navigation bar
+        # navbar
         navbar = QToolBar()
         self.addToolBar(navbar)
 
-        # the back button
         back_btn = QAction('Back', self)
         back_btn.triggered.connect(self.browser.back)
-        back_btn.setIcon(QIcon("back.png"))
-        back_btn.setToolTip("go backwards in your history")
         navbar.addAction(back_btn)
 
-        # the forward button
         forward_btn = QAction('Forward', self)
         forward_btn.triggered.connect(self.browser.forward)
-        forward_btn.setToolTip("go forwards in your history")
-        forward_btn.setIcon(QIcon("forward.png"))
         navbar.addAction(forward_btn)
 
-        # the reload button
         reload_btn = QAction('Reload', self)
         reload_btn.triggered.connect(self.browser.reload)
-        reload_btn.setIcon(QIcon("reload.png"))
-        reload_btn.setToolTip("Reload your currently opened page")
         navbar.addAction(reload_btn)
 
-        # the home button
         home_btn = QAction('Home', self)
-        home_btn.triggered.connect(self.nav_home)
-        home_btn.setIcon(QIcon("home.png"))
-        home_btn.setToolTip("go to your homepage")
+        home_btn.triggered.connect(self.navigate_home)
         navbar.addAction(home_btn)
 
-    def nav_home(self):
-        self.browser.setUrl(QUrl('http://duck.com'))
-   
+        self.url_bar = QLineEdit()
+        self.url_bar.returnPressed.connect(self.navigate_to_url)
+        navbar.addWidget(self.url_bar)
+
+        self.browser.urlChanged.connect(self.update_url)
+
+    def navigate_home(self):
+        self.browser.setUrl(QUrl('https://duckduckgo.com/'))
+
     def navigate_to_url(self):
         url = self.url_bar.text()
         self.browser.setUrl(QUrl(url))
-        self.url_bar = QLineEdit()
-        self.url_bar.returnPressend.connect(self.navigate_to_url)
-        navbar.addWidget(self.url_bar)
-        
+
+    def update_url(self, q):
+        self.url_bar.setText(q.toString())
 
 
 app = QApplication(sys.argv)
 QApplication.setApplicationName('PyBrowser')
-app.setWindowIcon(QIcon("Browser.png"))
 window = MainWindow()
 app.exec_()
